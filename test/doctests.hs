178683c1a80cc786e810d2f01b1cb30ef37c528e
Brad Bow <brad.bow@stacktracehq.com>
2019-05-06 11:09:50 +1000

    Add script for running single files doctests

--- test/doctests.hs
+++ test/doctests.hs
@@ -1,10 +1,18 @@
 import Test.DocTest
+import System.Environment
 
 main :: IO ()
-main = doctest
+main = doctest . getFileFromArgList =<< getArgs
+
+defaultFiles :: [String]
+defaultFiles =
   [ "-isrc"
   , "src/Lets/GetSetLens.hs"
   , "src/Lets/Lens.hs"
   , "src/Lets/OpticPolyLens.hs"
   , "src/Lets/StoreLens.hs"
   ]
+
+getFileFromArgList :: [String] -> [String]
+getFileFromArgList [] = defaultFiles
+getFileFromArgList (x:_) = ["-isrc", x]
