e8ab4c5e50b8cf5a626dfa9d028efe8b8baf5710
Brad Bow <brad.bow@stacktracehq.com>
2019-05-06 11:08:18 +1000

    Add hlint, hindent, ghcid to nix tools

--- shell.nix
+++ shell.nix
@@ -2,6 +2,11 @@
 let
   inherit (nixpkgs) pkgs;
   drv = import ./default.nix { inherit nixpkgs compiler; };
-  drvWithTools = pkgs.haskell.lib.addBuildDepends drv [ pkgs.cabal-install ];
+  drvWithTools = pkgs.haskell.lib.addBuildDepends drv [
+    pkgs.cabal-install
+    pkgs.hlint
+    pkgs.haskellPackages.hindent
+    pkgs.haskellPackages.ghcid
+  ];
 in
   if pkgs.lib.inNixShell then drvWithTools.env else drvWithTools
